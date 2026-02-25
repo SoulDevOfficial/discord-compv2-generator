512af1445e75465ec4ac0d2e9a529e4604bf3fd1
SoulDev <244410096+SoulDevOfficial@users.noreply.github.com>
2026-02-25 12:37:03 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-console.log('React mounting...');
+const App = () => {
+  return <h1>Hello from React!</h1>;
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+console.log("React is mounting...");
