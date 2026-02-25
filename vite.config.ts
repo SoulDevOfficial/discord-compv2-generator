a9450396b36ae5180781d29c74a32850b923ff45
SoulDev <244410096+SoulDevOfficial@users.noreply.github.com>
2026-02-25 11:39:08 -0500

    Fix since rep name changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/discord-components-v2-builder/",
+  base: "/discord-compv2-generator/",
 });
